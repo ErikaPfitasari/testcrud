0e3c66b8fefc92aabe9a0eb823a111299289fb6a
erikaprafitasari <erikaprafitasari1@gmail.com>
2017-03-10 21:12:22 +0700

    testcrud

---
+++ src/JOptionMessage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ERIKA-MOKLET
+ */
+class JOptionMessage {
+
+    static void showMessageDialog(frmMain aThis, String data_gagal_ditambahkan, String sukses, int WARNING_MESSAGE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
